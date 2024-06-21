9d15446c2e983eb960ac04d732ca14d07e261781
Anas Attoum <anasattoum.12321@gmail.com>
2024-06-21 17:19:54 +0300

    edits on responive header

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import styles from '../Styles/Header.module.css';
 import Logo from "../Components/Logo";
 
 export default function Header() {
+    const showListIcon = useRef()
+    const list = useRef()
 
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function () {
@@ -43,18 +45,40 @@ export default function Header() {
         })
     })
 
+    const showList = () => {
+        if (window.screen.width < 1000) {
+            list.current.classList.remove(styles.clickIconRemove)
+            list.current.classList.add(styles.clickIcon)
+
+            showListIcon.current.style.display = 'none'
+        }
+    }
+
+    const removeList = () => {
+        if (window.screen.width < 1000) {
+            showListIcon.current.style.display = 'inline-block'
+            list.current.classList.remove(styles.clickIcon)
+            list.current.classList.add(styles.clickIconRemove)
+        }
+    }
+
 
     return (
         <div id="header" className={styles.header}>
 
-            <Logo/>
+            <Logo />
 
             <div>
-                <div className={styles.iconResponsive}>
+                <div className={styles.iconResponsive} onClick={showList} ref={showListIcon}>
                     <div className={styles.icon}></div>
                 </div>
 
-                <div className={styles.listHeader}>
+                <div className={styles.listHeader} ref={list} onClick={removeList}>
+                    <div className={styles.iconResponsive2}>
+                    <div className={styles.frameIcon2}>
+                        <div className={styles.icon2}></div>
+                    </div>
+                    </div>
                     <div className={`home-nav ${styles.active}`}>
                         <a href="#home"><span>H </span><span>O </span><span>M </span><span>E</span></a>
                     </div>
@@ -64,13 +88,11 @@ export default function Header() {
                     </div>
 
                     <div className='skills-nav'>
-                        <a href="#skills"><span>S </span><span>K </span><span>I </span><span>L </span><span>L
-                        </span><span>S</span></a>
+                        <a href="#skills"><span>S </span><span>K </span><span>I </span><span>L </span><span>L </span><span>S</span></a>
                     </div>
 
                     <div className='projects-nav'>
-                        <a href="#projects"><span>P </span><span>R </span><span>O </span><span>J </span><span>E </span><span>C
-                        </span><span>T </span><span>S</span></a>
+                        <a href="#projects"><span>P </span><span>R </span><span>O </span><span>J </span><span>E </span><span>C </span><span>T </span><span>S</span></a>
                     </div>
 
                     <div className='contact-nav'>
