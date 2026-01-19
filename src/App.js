9fc1a2239fb0c81c473cb30eb7413a06cef24954
Anas Attoum <AnasAttoum.12321@gmail.com>
2026-01-19 21:32:30 +0300

    Redirect to the new portfolio

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 
-import { lazy, Suspense } from 'react';
-import Loading from './pages/Loading';
-import Footer from './components/Footer';
-
-const LazyMainPage = lazy(() => import("./pages/MainPage"));
-const LazyMoreDetailsPage = lazy(() => import("./pages/MoreDetails"));
-const LazyNotFoundPage = lazy(() => import("./pages/NotFound"));
+// const LazyMainPage = lazy(() => import("./pages/MainPage"));
+// const LazyMoreDetailsPage = lazy(() => import("./pages/MoreDetails"));
+// const LazyNotFoundPage = lazy(() => import("./pages/NotFound"));
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
 
-          <Route path='/' element={<Suspense fallback={<Loading />}><LazyMainPage /></Suspense>} />
-          <Route path='project/:projectName' element={<Suspense fallback={<Loading />}><LazyMoreDetailsPage /></Suspense>} />
+  useEffect(() => {
+    window.location.href = "https://anas-attoum.vercel.app/";
+  }, []);
 
-          <Route path='*' element={<Suspense fallback={<Loading />}><LazyNotFoundPage /></Suspense>} />
+  return null; // no need to render old routes
 
-        </Routes>
+  // return (
+  //   <>
+  //     <BrowserRouter>
+  //       <Routes>
+
+  //         <Route path='/' element={<Suspense fallback={<Loading />}><LazyMainPage /></Suspense>} />
+  //         <Route path='project/:projectName' element={<Suspense fallback={<Loading />}><LazyMoreDetailsPage /></Suspense>} />
+
+  //         <Route path='*' element={<Suspense fallback={<Loading />}><LazyNotFoundPage /></Suspense>} />
+
+  //       </Routes>
         
-        <Footer />
+  //       <Footer />
 
-      </BrowserRouter>
-    </>
-  );
+  //     </BrowserRouter>
+  //   </>
+  // );
 }
 
 export default App;
